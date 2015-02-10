4e00f6d002443d8f15e4f11c860ebaa21d819bb8
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-02-10 21:45:23 +0100

    Add Admin page and connect models

--- scripts/admin.py
+++ scripts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from scripts.models import Script, Review
 
-# Register your models here.
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
+
+class ScriptAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'changed_date', 'code']
+    fieldsets = [
+        (None,               {'fields': ['code']}),
+        ('Date information', {'fields': ['pub_date', 'changed_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ReviewInline]
+    list_display = ('code', 'pub_date', 'changed_date')
+
+admin.site.register(Script, ScriptAdmin)
+admin.site.register(Review)
