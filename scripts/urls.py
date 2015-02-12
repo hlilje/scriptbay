e7e4ef2301cb1374706f40f3c625f968dc7cb4f2
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-02-12 21:42:11 +0100

    Add basic view for scripts

--- scripts/urls.py
+++ scripts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
+
 from scripts import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
 )
--- scripts/views.py
+++ scripts/views.py
@@ -1,5 +1,28 @@
-from django.http import HttpResponse
+from django.views import generic
+from django.utils import timezone
+
+from scripts.models import Script, Review
 
 
-def index(request):
-    return HttpResponse("This is the script index.")
+class IndexView(generic.ListView):
+    template_name = 'scripts/index.html'
+    context_object_name = 'latest_script_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published scripts (not including those set to be
+        published in the future).
+        """
+        return Script.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Script
+    template_name = 'scripts/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any scripts that aren't published yet.
+        """
+        return Script.objects.filter(pub_date__lte=timezone.now())
