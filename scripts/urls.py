220fa29a15b5e3c434408007b4cd6871d776c63d
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-03-08 20:52:55 +0100

    Implement basic POST for form

--- scripts/urls.py
+++ scripts/urls.py
@@ -5,8 +5,6 @@ from scripts import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    # url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
-    # url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<script_id>\d+)/write_review/$', views.write_review, name='write_review'),
 )
--- scripts/views.py
+++ scripts/views.py
@@ -1,6 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from django.views import generic
 from django.views.generic.edit import FormView
@@ -27,7 +27,7 @@ class DetailView(FormView):
     model = Script
     template_name = 'scripts/detail.html'
     form_class = ReviewForm
-    # success_url = '/thanks/'
+    # success_url = '/thanks/' # TODO
 
     # Override GET to be able to pass the script ID to be used in the form
     def get(self, request, *args, **kwargs):
@@ -38,22 +38,18 @@ class DetailView(FormView):
         context_data['script'] = Script.objects.filter(id=kwargs['pk'])[0]
         return self.render_to_response(context_data)
 
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        # TODO Save form data
-        return super(DetailView, self).form_valid(form)
-
 
 def write_review(request, script_id):
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data) # TODO
-            print(script_id)
-            # return HttpResponseRedirect(reverse('scripts:detail', args=(p.id,)))
-            return HttpResponseRedirect(reverse('scripts:detail'))
+    script = get_object_or_404(Script, pk=script_id)
+    form = ReviewForm(request.POST) # TODO Remove?
+    if form.is_valid():
+        print(form.cleaned_data) # TODO
+        print(script_id)
+        # TODO Save the review
+        return render(request, 'scripts/detail.html', {'form': form,
+            'script': script,
+            'message': "Thanks for your review!"})
     else:
-        form = NameForm()
-
-    return render(request, 'scripts/detail.html', {'form': form})
+        return render(request, 'scripts/detail.html', {'form': form,
+            'script': script,
+            'error_message': "You must provide a valid comment."})
