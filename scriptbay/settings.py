ee8434c7a2ddbe2dd5b77a5da7b3d93d1e224b0b
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-05-12 22:04:07 +0200

    Add REST API

--- scriptbay/settings.py
+++ scriptbay/settings.py
@@ -69,9 +69,14 @@ DATABASES = {
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    # ]
+
+    # Only have API accessable for admins
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    # Turn on pagination
+    'PAGE_SIZE': 10
 }
 
 # Internationalization
